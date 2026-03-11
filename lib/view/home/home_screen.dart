75fc7c3482c4aef99b9d10f6b3da8756e405633b
Abdul Haseeb <abdulhaseeb.39393@gmail.com>
2026-03-11 16:53:51 +0500

    Add tabs in home screen to move between screens

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spend_wise/config/color/colors.dart';
+import 'package:spend_wise/viewModel/bloc/navigation/navigation_bloc.dart';
 
-import '../../viewModel/bloc/auth_state/auth_bloc.dart';
+import 'package:spend_wise/view/views.dart';
+import 'package:spend_wise/viewModel/bloc/theme/theme_bloc.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,21 +15,83 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  late NavigationBloc _navigationBloc;
+
+  final List<BottomNavigationBarItem> barItems = [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.arrow_circle_up),
+      label: 'Income',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.arrow_circle_down),
+      label: 'Expenses',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.handshake_outlined),
+      label: 'Loan',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.person_outline_rounded),
+      label: 'Profile',
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _navigationBloc = NavigationBloc();
+  }
+
+  @override
+  void dispose() {
+    _navigationBloc.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        actions: [
-          BlocBuilder<AuthBloc, AuthState>(
-            builder: (context, state) {
-              return
-                IconButton(onPressed: (){
-                  context.read<AuthBloc>().add(AuthLogOut());
-                }, icon: Icon(Icons.logout));
+    return BlocProvider(
+      create: (context) => _navigationBloc,
+      child: BlocBuilder<ThemeBloc,ThemeState>(
+        builder: (context, themeState) {
+          return BlocBuilder<NavigationBloc,NavigationState>(
+            builder: (context, navigationState) {
+              return Scaffold(
+                backgroundColor: themeState.theme[appColors.appBGColor],
+                  body: Column(
+                    children: [
+                      if (navigationState.selectedIndex != 3)
+                        const ProfileDataHeader(),
+                      Expanded(
+                        child: IndexedStack(
+                          index: navigationState.selectedIndex,
+                          children: [
+                            IncomeScreen(),
+                            ExpenseScreen(),
+                            LoanScreen(),
+                            ProfileScreen(),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  bottomNavigationBar: BottomNavigationBar(
+                    type: BottomNavigationBarType.fixed,
+                    currentIndex: navigationState.selectedIndex,
+                    backgroundColor: themeState.theme[appColors.primaryColor],
+                    unselectedItemColor: themeState.theme[appColors.accentColor],
+                    selectedItemColor: themeState.theme[appColors.cardColor],
+                    onTap: (index) {
+                      context.read<NavigationBloc>().add(ChangeIndex(index: index));
+                    },
+                    items: barItems,
+                  )
+              );
             }
-          )
-        ],
+          );
+        }
+
       ),
     );
   }
